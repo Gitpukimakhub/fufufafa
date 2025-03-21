c50db2e87ac7ea341c3d2d2d77f42eb1f35e54f7
Gitpukimakhub <kenny.egan47@gmail.com>
2025-03-21 23:48:28 +0700

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,33 @@ const upload = multer({ storage });
 
 app.use('/f', express.static(tmpDir));
 
+app.all('/', (req, res) => res.send(`<!DOCTYPE html>
+<html lang="id">
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+@font-face {
+  font-family: 'iAWriterDuospace';
+  src: url('iAWriterDuospace-Regular.woff2') format('woff2'),
+       url('iAWriterDuospace-Regular.woff') format('woff');
+  font-weight: normal;
+  font-style: normal;
+}
+body { font-family: 'iAWriterDuospace', sans-serif; margin: 20px; text-align: center; }
+input, button { margin: 10px; padding: 10px; }
+</style>
+</head>
+<body>
+<h1>File Uploader</h1>
+<hr>
+<p>No size limit</p>
+<form action="/upload" method="post" enctype="multipart/form-data">
+<input type="file" name="files[]" multiple>
+<button type="submit">Upload</button>
+</form>
+</body>
+</html>`));
+
 app.post('/upload', upload.single('files[]'), (req, res) => res.status(200).type('text').send(`https://${req.hostname}/f/${req.file.filename}`));
 
 app.listen(7860);
